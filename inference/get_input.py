3dbefa108030d40b5c752a8a1df2e558d4865bd5
Borna Ghahnoosh <borna.ahz@gmail.com>
2021-04-27 13:40:13 -0300

    Commit

--- inference/get_input.py
+++ inference/get_input.py
@@ -2,33 +2,9 @@ from flask import request
 from PIL import Image
 
 
-def verify_extension(f):
-    fname = f.filename
-    extension = fname.split('.')[-1]
-    allowed = ['jpg', 'jpeg', 'png']
-    if extension in allowed:
-        return True
-    return False
-
-
-def verify_size(img):
-    w, h = img.size
-    mx = max(w, h)
-    if 768 <= mx:
-        return False
-    return True
-
-
 def get_input():
-    print(request.files)
-    print(request.form)
     f = request.files['file']
-    if not verify_extension(f):
-        return 'extension', None, None
-
     img = Image.open(f).convert('RGB')
-    if not verify_size(img):
-        return 'size', None, None
 
     enhancement_level = int(request.form['enhancement_level'])
     enlarge = request.form.get('enlarge')
--- inference/main.py
+++ inference/main.py
@@ -21,19 +21,12 @@ def index():
 @app.route('/', methods=['POST'])
 def upload():
     img, enhancement_level, enlarge = get_input()
-
-    if img == 'extension':
-        flash('File has to be .jpg, .jpeg, or .png')
-        return redirect(url_for('index'))
-    
-    elif img == 'size':
-        flash('Maximum dimension of image must be under 768')
-        return redirect(url_for('index'))
-
     model = load_model().to(device)
+
     res, img = compare(model, img, enhancement_level, 
                        enlarge, device)
     collect()
+
     res = img_to_b64(res)
     img = img_to_b64(img)
     return render_template('index.html', img=img, res=res)
